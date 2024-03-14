45c0978e80b6a7ea40f5cb9cc6484e2d5bd9c090
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2024-03-14 10:30:29 +0100

    Added authentication controller

---
+++ BuberDinner.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.API.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var result = _authenticationService.Login(request.Email, request.Password);
+            var response = new AuthenticationResponse(result.Id, result.FirstName, result.LastName, result.Email, result.Password);
+            return Ok(response);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest request)
+        {
+            var result = _authenticationService.Register(request.FirstName, request.LastName, request.Email, request.Password);
+            var response = new AuthenticationResponse(result.Id, result.FirstName, result.LastName, result.Email, result.Password);
+            return Ok(response);
+        }
+    }
+}
