029e8a2814c0bce2bad445afe15885846bbbe1a7
Tomislav Periša <tomislav.pingi.perisa@gmail.com>
2024-03-14 14:16:48 +0100

    Added options pattern

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,14 @@
+using BuberDinner.Infrastructure.Authentication;
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace BuberDinner.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager config)
         {
+            services.Configure<JwtTokenOptions>(config.GetSection(JwtTokenOptions.SectionName));
             return services;
         }
     }
