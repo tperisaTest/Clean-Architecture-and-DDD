bcfd1f42fcf1f27c38047c81a834301aa6ac37ef
tperisa <tperisa@tis.hr>
2024-03-28 09:49:17 +0100

    Added ErrorOr package to domain layer and defined common domain errors

---
+++ BuberDinner.Domain/Common/Errors/Errors.User.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace BuberDinner.Domain.Common.Errors
+{
+    public static class Errors
+    {
+        public static class User
+        {
+            public static Error DuplicateEmail => Error.Conflict(code: "User.AlreadyExists", description: "User with this email already exists");
+            public static Error VoidUser => Error.NotFound(code: "User.IsVoid", description: "User does not exist.");
+        }
+    }
+}
